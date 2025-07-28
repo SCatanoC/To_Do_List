f0cb827451b1428424f27ed5648073065997b109
SCatanoC <santiago47c@gmail.com>
2025-07-28 09:43:11 -0500

    variables de entorno para conexión a base de datos

--- Back/db.js
+++ Back/db.js
@@ -1,15 +1,15 @@
-process.loadEnvFile();
+require("dotenv").config();
 
 // backend/db.js
 const mysql = require("mysql2");
 
 // conexión
 const db = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306, // sql port
+  port: process.env.DB_PORT,
 });
 
 //conect
